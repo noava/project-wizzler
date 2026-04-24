b3e80c881d7941cbe674b0920ad481b31b899a43
Kaalhode <kennyjdang123@gmail.com>
2026-04-24 06:36:08 +0200

    gecko + muskrat
    sound implemented to 2 animals, audio buses created
    
    Generally, you should pick:
        ogg if you care most about file size, but it will use more CPU to decompress. Generally, that's what you want.
        wav if you have storage space but care most about decompression CPU usage, for example on very low-powered devices.
        mp3 is somewhere in the middle; not as compressed as ogg, but also not as CPU intensive.

--- Animals/Models/Gecko/gecko.gd
+++ Animals/Models/Gecko/gecko.gd
@@ -7,3 +7,10 @@ func _ready() -> void:
 		var material = mesh.get_active_material(0).duplicate()
 		material.albedo_color = color_modulation
 		mesh.set_surface_override_material(0, material)
+	animal_sound()
+
+func animal_sound():
+	await get_tree().create_timer(randf_range(1.0,5.0)).timeout
+	$AudioStreamPlayer3D.pitch_scale = randf_range(0.7,1)
+	$AudioStreamPlayer3D.play()
+	animal_sound()
