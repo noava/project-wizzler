33819e9ef5c758bd7a736b892c575f4fbc8e2b95
Kaalhode <kennyjdang123@gmail.com>
2026-05-08 02:25:02 +0200

    testing
    sound didnt play as expected, which was weird. but using prints gave 3 different values on the terrain. Terrain ID(2.0, 0.0, 0.891379) when walking on the mountain parts which indicates x value is the material id, while the other values are something else.

--- Player/player_movement.gd
+++ Player/player_movement.gd
@@ -14,23 +14,37 @@ const SENSITIVITY = 0.004
 #have to differentiate sounds between crouch, walk and sprinting/running
 
 var texture_sounds = {
-	0: preload("res://Sounds/Material/grass.wav")
+    0: preload("res://Sounds/Material/grass.wav")
 }
 
 var footstep_timer := 0.0
 var footstep_interval := 0.4
 
 func _physics_process(delta: float) -> void:
-	if velocity.length() > 0.1 and is_on_floor():
-		footsteps_handle(delta)
+    if velocity.length() > 0.1 and is_on_floor():
+        footsteps_handle(delta)
 
 func footsteps_handle(delta):
-	footstep_timer -= delta
-	
-	if footstep_timer <= 0.0:
-		footstep_timer = footstep_interval
-		play_footstep()
+    footstep_timer -= delta
+    
+    if footstep_timer <= 0.0:
+        footstep_timer = footstep_interval
+        play_footstep()
 
+func play_footstep():
+    if terrain == null:
+        return
+    
+    var texture_id = terrain.data.get_texture_id(global_position)
+    print("Terrain ID", texture_id)
+    if texture_sounds.has(texture_id):
+        print("has sound", texture_sounds.has(texture_id))
+        audio.stream = texture_sounds[texture_id]
+        if audio.stream:
+            print(audio.stream.resource_path)
+        else:
+            print("No audio")
+        audio.play()
 
 # Crouch
 var is_crouching = false
@@ -44,44 +58,44 @@ var gravity = 9.8
 var movement_lock = false
 
 func _process(delta):
-	if movement_lock: return
+    if movement_lock: return
 
-	# Gravity
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+    # Gravity
+    if not is_on_floor():
+        velocity.y -= gravity * delta
 
-	# Jump
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
-	# Crouch (hold)
-	is_crouching = Input.is_action_pressed("crouch")
-	
-	# Crouch (toggle) Uncomment code below for toggle crouch TODO: Make this as a toggle in settings
-	#if Input.is_action_just_pressed("crouch"):
-	#	is_crouching = !is_crouching
-		
-	# Sprint
-	if Input.is_action_pressed("sprint") and not is_crouching:
-		speed = SPRINT_SPEED
-	elif is_crouching:
-		speed = CROUCH_SPEED
-	else:
-		speed = WALK_SPEED
-		
-	# Movement/Deceleration.
-	var input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction = (head.transform.basis * transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
-	
-	if is_on_floor():
-		if direction:
-			velocity.x = direction.x * speed
-			velocity.z = direction.z * speed
-		else:
-			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
-			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
-	else:
-		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
-		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
-	
-	move_and_slide()
+    # Jump
+    if Input.is_action_just_pressed("jump") and is_on_floor():
+        velocity.y = JUMP_VELOCITY
+        
+    # Crouch (hold)
+    is_crouching = Input.is_action_pressed("crouch")
+    
+    # Crouch (toggle) Uncomment code below for toggle crouch TODO: Make this as a toggle in settings
+    #if Input.is_action_just_pressed("crouch"):
+    #	is_crouching = !is_crouching
+        
+    # Sprint
+    if Input.is_action_pressed("sprint") and not is_crouching:
+        speed = SPRINT_SPEED
+    elif is_crouching:
+        speed = CROUCH_SPEED
+    else:
+        speed = WALK_SPEED
+        
+    # Movement/Deceleration.
+    var input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+    var direction = (head.transform.basis * transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
+    
+    if is_on_floor():
+        if direction:
+            velocity.x = direction.x * speed
+            velocity.z = direction.z * speed
+        else:
+            velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+            velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+    else:
+        velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+        velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+    
+    move_and_slide()
